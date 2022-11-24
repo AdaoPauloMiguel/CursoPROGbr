c9564d00d3142dc2dc6c7775fefecb89adcecb1b
AdaoPauloMiguel <104330874+AdaoPauloMiguel@users.noreply.github.com>
2022-11-24 21:58:53 +0300

    Js Basic
    Aprendendo o basico de Js

---
+++ Aulas/Javascript-básico/script.js
@@ -0,0 +1,74 @@
+console.log('Hello word')
+
+// Variaveis
+
+var a = 'Placa de captura'
+
+console.log(a)
+
+// Tipos de dados
+
+// Boolean
+// NUll
+// Number
+// String
+
+var a = 'Qual quer coisa'
+var b = 2
+var c = true
+var d = null
+
+console.log()
+
+          // Operações Aritimétricas
+
+// + Adição
+// - Subtração
+// * Multiplicação
+// / divisão
+// % Modulo
+// ++ Incremento
+// -- Decremento
+
+            // Atribuições
+
+// =   x = y    x = y
+// +=  x += y   x = x + y
+// -=  x -= y   x = x - y
+// *=  x *= y   x = x * y
+// /=  x /= y   x = x / y
+// %=  x %= y   x = x % y
+
+
+
+var a = 30
+
+// a = a**5
+
+a **= 5
+
+console.log(a)
+
+
+            // Operadores Comparativos.
+
+// == Igual a valor
+// ==== Igual a valor e tipo
+// != Diferente de valor 
+// !== Diferente de valor e tipo
+// > Maior que 
+// < Menor que 
+// >= Maior ou igual que 
+// <= Menor ou igual que 
+
+var a = 3
+var b = 4
+
+console.log(a > b)
+
+            // Operadores Comparativos.
+      
+// && e 
+// || ou 
+// ! Negativo 
+
