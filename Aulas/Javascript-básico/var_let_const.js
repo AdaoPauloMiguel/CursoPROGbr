d27cc7b4fb253b8662faef757ac2ce0c1f01df2d
AdaoPauloMiguel <104330874+AdaoPauloMiguel@users.noreply.github.com>
2024-06-23 02:26:30 +0300

    Terminando o módulo de JS básico.
    De volta as minhas aulas do curso Programador BR.
    
    Terminei o módulo básico falando sobre Var, Let e Const.
    
    Aprendi conceitos de escopos local e global e Const pode ser modificada mas não reatribuída.

---
+++ Aulas/Javascript-básico/var_let_const.js
@@ -0,0 +1,8 @@
+var num1 = 4  // A variavel pode ser reatribuida.(Variável Global) 
+let num2 = 5 // A diferença do let para o var é o escopo, o let tem escopo local ao criar ele, só existe dentro do bloco. Já o var tem escopo global. (Variável local)
+const num3 = 6 // Const pode ser modificada mas não reatribuida. (Constante local)
+
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
